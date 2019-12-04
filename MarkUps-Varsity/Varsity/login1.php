b2011751cf4eed788cec2c4f3e171a05d0297708
episkuswono <episkuswono@gmail.com>
2019-12-04 09:53:26 +0700

    login admin design finis

--- MarkUps-Varsity/Varsity/login1.php
+++ MarkUps-Varsity/Varsity/login1.php
@@ -34,35 +34,31 @@
    <head>
       <title>Login Page</title>
       
-      <style type = "text/css">
-         body {
-            font-family:Arial, Helvetica, sans-serif;
-            font-size:14px;
-         }
-         label {
-            font-weight:bold;
-            width:100px;
-            font-size:14px;
-         }
-         .box {
-            border:#666666 solid 1px;
-         }
-      </style>
+    <link href="assets/css/font-awesome.css" rel="stylesheet">
+    <!-- Bootstrap -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">   
+    <!-- Slick slider -->
+    <link rel="stylesheet" type="text/css" href="assets/css/slick.css">          
+    <!-- Fancybox slider -->
+    <link rel="stylesheet" href="assets/css/jquery.fancybox.css" type="text/css" media="screen" /> 
+    <!-- Theme color -->
+    <link id="switcher" href="assets/css/theme-color/default-theme.css" rel="stylesheet"> 
+    <link id="forlogin" href="assets/css/login.css" rel="stylesheet"/>
       
-   </head>
+   </head><br><br><br><br>
    
    <body bgcolor = "#FFFFFF">
 	
       <div align = "center">
          <div style = "width:300px; border: solid 1px #333333; " align = "left">
-            <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
+            <div style = "background-color:#333333; color:#FFFFFF; padding:3px;" align = "center"><b>Login Admin</b></div>
 				
-            <div style = "margin:30px">
+            <div style = "margin:10px">
                
-               <form action = "" method = "post">
-                  <label>username  :</label><input type = "text" name = "username" class = "box"/><br /><br />
-                  <label>password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
-                  <input type = "submit" value = " Submit "/><br />
+               <form class="form-signin" METHOD="POST">        
+                  <label>username  :</label><input type = "text" name = "username" class = "form-control"/>
+                  <label>password  :</label><input type = "password" name = "password" class = "form-control"/><br>
+                  <button class="btn btn-lg btn-primary btn-block" type="submit" name="submit" value="Log in">Submit</button>
                </form>
                
                <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
