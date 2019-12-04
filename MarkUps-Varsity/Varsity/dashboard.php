d5780c34f7270d40e4b8e665089cf76f045879d7
episkuswono <episkuswono@gmail.com>
2019-12-04 10:32:21 +0700

    in

--- MarkUps-Varsity/Varsity/dashboard.php
+++ MarkUps-Varsity/Varsity/dashboard.php
@@ -1,7 +1,3 @@
-
-
-
-
 <?php
     include('session.php');
     // session_start();
@@ -28,7 +24,7 @@
 	<link rel="icon" type="image/png" href="assets/img/favicon.ico">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-	<title>Light Bootstrap Dashboard by Creative Tim</title>
+	<title>Dashboard Creative Tim</title>
 
 	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
